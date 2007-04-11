0a4727ca913db3e56df584b527aa72c7c94347a5
msilva <msilva@aa2f973e-fc12-0410-ae6e-85b500b3c72b>
2007-04-11 20:20:58 +0000

    - Trying to publish the modification using a web service. Up to now, this was kidda unsuccessful.

--- slapi-plugin-publisher/slapi-plugin-publisher.c
+++ slapi-plugin-publisher/slapi-plugin-publisher.c
@@ -19,17 +19,116 @@
 #include <lber.h>
 
 #include <slapi-plugin.h>
+#include <ws/soapClient.c>
 
-int addrdnvalues_preop_init(Slapi_PBlock *pb);
-
-static Slapi_PluginDesc pluginDescription = {
+static Slapi_PluginDesc modification_publisher_plugin_description = {
 	"modification-publisher",
 	"ideais.sf.net",
 	"1.0",
 	"Publishes any modification in the LDAP server"
 };
 
-static int addrdnvalues_preop_add(Slapi_PBlock *pb)
+static int modification_publisher_publish_modification()
+{
+	struct _ns1__postMsgToQueue message;
+	struct _ns1__postMsgToQueueResponse response;
+	struct soap *soap; // gSOAP runtime environment
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "Begin");
+
+	message.in0 = "queue/testQueue";
+	message.in1 = "teste message";
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "a");
+	soap = soap_new();
+	if (!soap) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "Could not create the SOAP handler");
+	}
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "ab");
+	soap_call___ns1__postMsgToQueue(soap, NULL, NULL, &message, &response);
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "abc");
+	soap_destroy(soap);
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "abcd");
+	soap_end(soap);
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "abcde");
+	soap_done(soap);
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Publish modification", "Done");
+}
+
+static int modification_publisher_postop_add(Slapi_PBlock *pb)
+{
+	Slapi_Entry *e;
+	char *dn;
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Post-operation add plugin", "Begin");
+
+	if (slapi_pblock_get(pb, SLAPI_ADD_ENTRY, &e) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, (char *) __func__, "Error retrieving target entry\n");
+		return -1;
+	}
+	
+	if (slapi_pblock_get(pb, SLAPI_ADD_TARGET, &dn) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, (char *) __func__, "Error retrieving target distinguished name\n");
+		return -1;
+	}
+	slapi_dn_normalize_case(dn);
+	slapi_log_error(SLAPI_LOG_PLUGIN, (char *) __func__, "Normalized DN: %s\n", dn ); 
+
+	modification_publisher_publish_modification();
+
+	return LDAP_SUCCESS;
+}
+
+static int modification_publisher_postop_modify(Slapi_PBlock *pb)
+{
+	int rc;
+	LDAPMod **mods;
+	Slapi_Mod *cur;
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Post-operation modify plugin", "Begin");
+
+	if (slapi_pblock_get(pb, SLAPI_MODIFY_MODS, &mods) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, (char *) __func__, "Error retrieving modifications\n");
+		return -1;
+	}
+
+	modification_publisher_publish_modification();
+
+
+/*
+	for (cur = *mods; cur != NULL; cur++) {
+		cur->mod_type; // Attribute type
+		// cur->mod_values
+		switch (cur->mod_op) {
+			case LDAP_MOD_ADD:
+				break;
+			case LDAP_MOD_DELETE:
+				break;
+			case LDAP_MOD_REPLACE:
+				break;
+		}
+	}
+	rc = slapi_entry_add_rdn_values(e);
+	if (rc != LDAP_SUCCESS) {
+		slapi_send_ldap_result(pb, LDAP_OTHER, NULL,
+			"Failed to parse distinguished name", 0, NULL);
+		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_add",
+			"Failed to parse distinguished name: %s\n",
+			ldap_err2string(rc));
+		return -1;
+	}
+*/
+
+	return 0;
+}
+
+
+static int modification_publisher_postop_modify_rdn(Slapi_PBlock *pb)
 {
 	int rc;
 	Slapi_Entry *e;
@@ -54,40 +153,71 @@ static int addrdnvalues_preop_add(Slapi_PBlock *pb)
 	return 0;
 }
 
-int addrdnvalues_preop_init(Slapi_PBlock *pb)
+
+static int modification_publisher_postop_delete(Slapi_PBlock *pb)
 {
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_VERSION, SLAPI_PLUGIN_VERSION_03) != 0) {
-	       return -1;
-	}
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_DESCRIPTION, &pluginDescription) != 0) {
+	int rc;
+	Slapi_Entry *e;
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Post-operation add plugin", "Begin");
+
+	if (slapi_pblock_get(pb, SLAPI_ADD_ENTRY, &e) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_add", "Error retrieving target entry\n");
 		return -1;
 	}
 
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_ADD_FN, (void *)addrdnvalues_preop_add) != 0) {
-		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_init", "Error registering %s\n", pluginDescription.spd_description);
-		return -1;
-	}
-
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_MODIFY_FN, (void *)addrdnvalues_preop_add) != 0) {
-		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_init", "Error registering %s\n", pluginDescription.spd_description);
-		return -1;
-	}
-
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_MODRDN_FN, (void *)addrdnvalues_preop_add) != 0) {
-		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_init", "Error registering %s\n", pluginDescription.spd_description);
-		return -1;
-	}
-
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_DELETE_FN, (void *)addrdnvalues_preop_add) != 0) {
-		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_init", "Error registering %s\n", pluginDescription.spd_description);
-		return -1;
-	}
-
-	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_ABANDON_FN, (void *)addrdnvalues_preop_add) != 0) {
-		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_init", "Error registering %s\n", pluginDescription.spd_description);
+	rc = slapi_entry_add_rdn_values(e);
+	if (rc != LDAP_SUCCESS) {
+		slapi_send_ldap_result(pb, LDAP_OTHER, NULL,
+			"Failed to parse distinguished name", 0, NULL);
+		slapi_log_error(SLAPI_LOG_PLUGIN, "addrdnvalues_preop_add",
+			"Failed to parse distinguished name: %s\n",
+			ldap_err2string(rc));
 		return -1;
 	}
 
 	return 0;
 }
 
+
+int modification_publisher_init(Slapi_PBlock *pb)
+{
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Modification publisher plugin", "Loading");
+
+
+	if (slapi_pblock_set(pb, SLAPI_PLUGIN_VERSION, SLAPI_PLUGIN_VERSION_03) != 0) {
+		goto error;
+	}
+	
+	if (slapi_pblock_set(pb, SLAPI_PLUGIN_DESCRIPTION, &modification_publisher_plugin_description) != 0) {
+		goto error;
+	}
+
+	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_ADD_FN, (void *)modification_publisher_postop_add) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, "modification_postop_add", "Error registering %s\n", modification_publisher_plugin_description.spd_id);
+		goto error;
+	}
+
+	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_MODIFY_FN, (void *)modification_publisher_postop_modify) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, "modification_postop_modify", "Error registering %s\n", modification_publisher_plugin_description.spd_id);
+		goto error;
+	}
+
+	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_MODRDN_FN, (void *)modification_publisher_postop_modify_rdn) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, "modification_postop_modify_rdn", "Error registering %s\n", modification_publisher_plugin_description.spd_id);
+		goto error;
+	}
+
+	if (slapi_pblock_set(pb, SLAPI_PLUGIN_POST_DELETE_FN, (void *)modification_publisher_postop_delete) != 0) {
+		slapi_log_error(SLAPI_LOG_PLUGIN, "modification_postop_delete", "Error registering %s\n", modification_publisher_plugin_description.spd_id);
+		goto error;
+	}
+
+
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Modification publisher plugin", "Initialialized");
+	return 0;
+
+error:
+	slapi_log_error(SLAPI_LOG_PLUGIN, "Modification publisher plugin", "Load aborted");
+	return -1;
+}
