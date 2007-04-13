8aa87d8069a6a37024a87cca5c7c76d8d2162314
msilva <msilva@aa2f973e-fc12-0410-ae6e-85b500b3c72b>
2007-04-13 15:33:20 +0000

    - Now the libraries and flags required to build the tests are discovered at runtime (yeah, die hardcoded configuration!). - Removed bogus code from soap_initialization (not actually that futile code, but it belogs to post_message_queue). - Implemented the post_message_queue. And it works!

--- slapi-plugin-publisher/tests/soap_initialization.c
+++ slapi-plugin-publisher/tests/soap_initialization.c
@@ -4,25 +4,13 @@
 
 int main()
 {
-	struct _ns1__postMsgToQueue message;
-	struct _ns1__postMsgToQueueResponse response;
 	struct soap *soap; // gSOAP runtime environment
-
-	message.in0 = "queue/testQueue";
-	message.in1 = "teste message";
 
 	soap = soap_new();
 	if (!soap) {
 		return 1;
 	}
 
-	/*
-	if (*soap.error) {
-		soap_print_fault(soap, stderr);
-		exit(1);
-	}
-	*/
-	
 	soap_destroy(soap);
 	soap_end(soap);
 	soap_done(soap);
